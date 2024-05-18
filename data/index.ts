6f4d3231e06cf6da743b136b340da3a2632e5840
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-05-18 09:04:11 +0530

    New

--- data/index.ts
+++ data/index.ts
@@ -108,7 +108,7 @@ export const testimonials = [
   {
     quote:
       "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Harsh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Harsh is the ideal partner.",
-    name: "Michael Johnson",
+    name: "Michael K.",
     title: "Director of AlphaStream Technologies",
   },
   {
